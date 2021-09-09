63a10db6c00eaaf29f02bece009c201a3e1bfdae
Federico Cavalieri <me@fcavalieri.com>
2021-09-09 14:28:01 +0200

    Added scripts

---
+++ release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+set -e
+set -u
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+RELEASE_DIR="$SCRIPT_DIR/release"
+rm -rf "$RELEASE_DIR"
+mkdir -p "$RELEASE_DIR"
+
+(
+  cd "$SCRIPT_DIR/core/src/main/resources/org/jobrunr/dashboard/frontend"
+  . ~/.nvm/nvm.sh
+  nvm use 12
+  npm install
+  npm run build
+)
+
+(
+  cd "$SCRIPT_DIR"
+  export SDKMAN_DIR="$HOME/.sdkman"
+  set +u
+  . "$HOME/.sdkman/bin/sdkman-init.sh"
+  sdk use java 11.0.8-open
+  set -x
+  ./gradlew publish
+)
